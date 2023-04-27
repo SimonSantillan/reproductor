7fab4783e15aa257d601cae6ba2851f961246a53
SimonSantillan <sgeronimosantillan@outlook.com>
2023-04-27 18:03:50 -0300

    Mi reproductor

---
+++ js/music-play.js
@@ -0,0 +1,92 @@
+let musicArray = [
+  "music/eurythmics-sweet-dreams.mp3",
+  "music/klan-no-drama.mp3",
+  "music/antwan-no-hay-pausa.mp3",
+  "music/sank-nueva-piel.mp3",
+  "music/sank-animality.mp3",
+  "music/sank-ahora-o-nunca"
+];
+
+let onOf = true;
+
+let imgReproductor = document.querySelector(".img-reproductor");
+let previous = document.querySelector(".previous");
+let playPause = document.querySelector(".play-pause");
+let next = document.querySelector(".next");
+let stopM = document.querySelector(".stopM");
+let playPauseImg = document.querySelector(".play-pause-img");
+
+let theMusic = document.getElementById("the-music");
+let musicPosition = 0;
+
+function playing() {
+  if (onOf) {
+    onOf = false;
+    imgReproductor.src = "bg-move/dog.webp";
+    playPauseImg.src = "buttons/pause.svg";
+    music();
+  } else {
+    onOf = true;
+    imgReproductor.src = "bg-move/dog.jpg"
+    playPauseImg.src = "buttons/play.svg";
+    music();
+  }
+}
+
+let oneTime = true;
+function music() {
+  if (theMusic.paused && oneTime) {
+    theMusic.src = musicArray[musicPosition]
+    oneTime = false;
+  }
+  if (theMusic.paused) {
+    theMusic.play();
+  }
+  else {
+    theMusic.pause();
+  }
+}
+
+function previousAndNext(e) {
+  if (!onOf) {
+    if (e.target.alt === "next") {
+      if (musicArray[musicPosition] === musicArray[musicArray.length - 1]) {
+        musicPosition = 0;
+        theMusic.src = musicArray[musicPosition];
+        music();
+      } else {
+        musicPosition++;
+        theMusic.src = musicArray[musicPosition];
+        music();
+      }
+    } else if (e.target.alt === "previous") {
+      if (musicArray[musicPosition] === musicArray[0]) {
+        musicPosition = (musicArray.length - 1);
+        theMusic.src = musicArray[musicPosition];
+        music();
+      } else {
+        musicPosition--;
+        theMusic.src = musicArray[musicPosition];
+        music();
+      }
+    }
+  }
+}
+
+function stopMusic() {
+  if (!theMusic.paused) {
+    imgReproductor.src = "bg-move/dog.jpg";
+    theMusic.pause();
+    theMusic.currentTime = 0;
+    console.log("apretando")
+    onOf = true;
+    playPauseImg.src = "buttons/play.svg";
+  } else {
+    theMusic.currentTime = 0;
+  }
+}
+
+playPause.addEventListener("click", playing);
+next.addEventListener("click", previousAndNext);
+previous.addEventListener("click", previousAndNext);
+stopM.addEventListener("click", stopMusic);
